87b28ddbae589ca0dea36a399156d68c4f2b44ed
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-04-26 11:39:37 +0200

    CurrencyCodeTableC findByName tests added

--- src/test/java/com/komfortcieplny/NBPExchangeRatesViewer/model/CurrencyCodeTableCTest.java
+++ src/test/java/com/komfortcieplny/NBPExchangeRatesViewer/model/CurrencyCodeTableCTest.java
@@ -5,33 +5,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CurrencyCodeTableATest {
+class CurrencyCodeTableCTest {
 
     @Test
     @DisplayName("Should Return True When Valid Code Is Provided")
     void shouldReturnTrueWhenValidCodeIsProvided() {
-        boolean result = CurrencyCodeTableA.findByName("USD");
+        boolean result = CurrencyCodeTableC.findByName("USD");
         assertTrue(result);
     }
 
     @Test
     @DisplayName("Should Return True When Valid LowerCase Code Is Provided")
     void shouldReturnTrueWhenValidLowerCaseCodeIsProvided() {
-        boolean result = CurrencyCodeTableA.findByName("usd");
+        boolean result = CurrencyCodeTableC.findByName("usd");
         assertTrue(result);
     }
 
     @Test
     @DisplayName("Should Return True When Valid MixedCase Code Is Provided")
     void shouldReturnTrueWhenValidMixedCaseCodeIsProvided() {
-        boolean result = CurrencyCodeTableA.findByName("uSd");
+        boolean result = CurrencyCodeTableC.findByName("uSd");
         assertTrue(result);
     }
 
     @Test
     @DisplayName("Should Return False When Invalid Code Is Provided")
     void shouldReturnFalseWhenInvalidCodeIsProvided() {
-        boolean result = CurrencyCodeTableA.findByName("us");
+        boolean result = CurrencyCodeTableC.findByName("us");
         assertFalse(result);
     }
 }
