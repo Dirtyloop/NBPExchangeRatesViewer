21bbeecc0e21c757da27fdfa5d1d5d5bc35b53a3
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-04-26 09:07:12 +0200

    getAverageExchangeRate method added

--- src/main/java/com/komfortcieplny/NBPExchangeRatesViewer/controller/ExchangeRatesController.java
+++ src/main/java/com/komfortcieplny/NBPExchangeRatesViewer/controller/ExchangeRatesController.java
@@ -1,9 +1,22 @@
 package com.komfortcieplny.NBPExchangeRatesViewer.controller;
 
+import com.komfortcieplny.NBPExchangeRatesViewer.service.ExchangeRatesService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/exchangerates")
 public class ExchangeRatesController {
+    private final ExchangeRatesService exchangeRatesService;
+
+    public ExchangeRatesController(ExchangeRatesService exchangeRatesService) {
+        this.exchangeRatesService = exchangeRatesService;
+    }
+
+    @GetMapping("/average/{code}/{effectiveDate}")
+    public String getAverageExchangeRete(@PathVariable String code, @PathVariable String effectiveDate) {
+        return exchangeRatesService.getAverageExchangeRate(code, effectiveDate);
+    }
 }
--- src/main/java/com/komfortcieplny/NBPExchangeRatesViewer/service/ExchangeRatesService.java
+++ src/main/java/com/komfortcieplny/NBPExchangeRatesViewer/service/ExchangeRatesService.java
@@ -4,4 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ExchangeRatesService {
+    public String getAverageExchangeRate(String code, String effectiveDate) {
+        return nbpClient.getAverageExchangeRate(code, effectiveDate);
 }
