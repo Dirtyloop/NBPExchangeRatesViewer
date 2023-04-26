0c87f3e15d3cea4437aebd21c1b280cfabf6a883
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-04-26 07:10:36 +0200

    initial commit

---
+++ src/main/java/com/komfortcieplny/NBPExchangeRatesViewer/NbpExchangeRatesViewerApplication.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.NBPExchangeRatesViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpExchangeRatesViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpExchangeRatesViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komfortcieplny/NBPExchangeRatesViewer/NbpExchangeRatesViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.NBPExchangeRatesViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpExchangeRatesViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
